53da87b8bfba6dabf7d5ed86b6587f94e9ae09fb
NakagamiYuta <guguru0014@gmail.com>
2021-04-10 22:10:22 +0900

    [add] とりあえずGUIが立ち上がるようにした

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 SRCS := $(wildcard src/*.java src/*/*.java)
+JAVAFX_MODULES := javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web
 
 ARGS =
+OPTS := -p $(JAVAFX_PATH)/lib --add-modules $(JAVAFX_MODULES)
 JAVA_OPTS := $(OPTS) -classpath bin
 JAVAC_OPTS := $(OPTS) -sourcepath src -d bin
 
 run: Main.class
+	cp -r src/fxml bin
 	java $(JAVA_OPTS) Main $(ARGS)
 
 Main.class: $(SRCS)
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
-import java.util.ArrayList;
+import java.io.IOException;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import controller.MainUIController;
+
+public class Main extends Application {
 
     public static void main(String args[]) {
-        System.out.println("Hello Electrody!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Scene
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+        loader.setController(new MainUIController());
+        Scene scene = new Scene(loader.load());
+
+        // Stage
+        stage.setTitle("Electrody");
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
