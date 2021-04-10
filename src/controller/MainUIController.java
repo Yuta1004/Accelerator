43b1e88f2a763f09efc459b37d63f34db247232c
NakagamiYuta <guguru0014@gmail.com>
2021-04-11 00:41:53 +0900

    [change] DisplayBuilderを変更メソッドをMainUIControllerに実装

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -5,11 +5,40 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SubScene;
+import javafx.scene.layout.AnchorPane;
+
+import builder.DisplayBuilder;
 
 public class MainUIController implements Initializable {
 
+    // UI部品
+    @FXML
+    private AnchorPane displayPane;
+
+    // 描画用
+    private DisplayBuilder dbuilder;
+
     @Override
     public void initialize(URL location, ResourceBundle resource) {
+    }
+
+    /**
+     * 画面描画構成の変更を行う
+     */
+    private void changeDBuilder(DisplayBuilder newBuilder) {
+        SubScene oldScene = dbuilder == null ? null : dbuilder.getScene();
+        SubScene newScene = newBuilder.getScene();
+
+        // AnchorPane設定 (全方向のAnchor->0.0)
+        AnchorPane.setTopAnchor(newScene, 0.0);
+        AnchorPane.setBottomAnchor(newScene, 0.0);
+        AnchorPane.setLeftAnchor(newScene, 0.0);
+        AnchorPane.setRightAnchor(newScene, 0.0);
+        displayPane.getChildren().remove(oldScene);
+        displayPane.getChildren().add(newScene);
+
+        dbuilder = newBuilder;
     }
 
 }
