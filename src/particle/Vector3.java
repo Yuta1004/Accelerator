039551466ca8f19ff8a25e31feab7892331bda9d
NakagamiYuta <guguru0014@gmail.com>
2021-04-10 17:16:22 +0900

    [add] Vector3

---
+++ src/particle/Vector3.java
@@ -0,0 +1,13 @@
+package src.particle;
+
+public class Vector3 {
+
+    public double x, y, z;
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.x = z;
+    }
+
+}
