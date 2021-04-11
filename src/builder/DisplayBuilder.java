6b1cbe2f19251333a5aa6fa6fe9e01b73e64b891
NakagamiYuta <guguru0014@gmail.com>
2021-04-12 02:37:34 +0900

    [change] DisplayBuilder.set2DCameraの引数名を変更

--- src/builder/DisplayBuilder.java
+++ src/builder/DisplayBuilder.java
@@ -7,7 +7,7 @@ import particle.ParticleManager;
 public interface DisplayBuilder {
 
     public SubScene getScene();
-    public void set2DCamera(double widthS, double widthF, double heightS, double heightF);
+    public void set2DCamera(double horizontalS, double horizontalF, double verticalS, double verticalF);
     public void set3DCamera(double x, double y, double z, double rh, double rv);
     public void update(ParticleManager pmanager);
     public void reset();
