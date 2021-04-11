f0a20152652a831cd82a8b593fe26fddfb7361a8
NakagamiYuta <guguru0014@gmail.com>
2021-04-11 16:30:36 +0900

    [add] DisplayBuilderにset2D/3DCameraを追加

--- src/builder/DisplayBuilder.java
+++ src/builder/DisplayBuilder.java
@@ -9,6 +9,8 @@ import particle.Particle;
 public interface DisplayBuilder {
 
     public SubScene getScene();
+    public void set2DCamera(double widthS, double widthF, double heightS, double heightF);
+    public void set3DCamera(double x, double y, double z, double rh, double rv);
     public void update(ArrayList<Particle> particles);
 
 }
