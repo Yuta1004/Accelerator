225039d8f402163e6dc91aff248d16f92329e4c6
NakagamiYuta <guguru0014@gmail.com>
2021-04-11 23:47:25 +0900

    [fix] 粒子表示ListView内の電子/陽子表示ミスを修正

--- src/controller/ParticleListCell.java
+++ src/controller/ParticleListCell.java
@@ -17,7 +17,7 @@ public class ParticleListCell extends ListCell<ParticleStatData> {
 
     @FXML private HBox root;
     @FXML private Circle circle;
-    @FXML private Label idLabel, initialPosLabel, initialVeloLabel;
+    @FXML private Label name, idLabel, initialPosLabel, initialVeloLabel;
 
     @Override
     public void updateItem(ParticleStatData data, boolean empty) {
@@ -47,6 +47,7 @@ public class ParticleListCell extends ListCell<ParticleStatData> {
 
         // ラベル指定
         Vector3 pos = data.initialPosition, velocity = data.initialVelocity;
+        name.setText(data.isElectron ? "電子" : "陽子");
         idLabel.setText(String.format("%02d", data.id));
         initialPosLabel.setText(String.format("(%.2f, %.2f, %.2f)", pos.x, pos.y, pos.z));
         initialVeloLabel.setText(String.format("(%.2f, %.2f, %.2f)", velocity.x, velocity.y, velocity.z));
