b80c47df31b776979243b6f445f262147e409b34
NakagamiYuta <guguru0014@gmail.com>
2021-04-11 18:04:25 +0900

    [add] 3DカメラをUIから操作できるようにした

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -2,11 +2,13 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.lang.Runnable;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SubScene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Slider;
 import javafx.animation.Timeline;
 import javafx.animation.KeyFrame;
 import javafx.util.Duration;
@@ -18,8 +20,8 @@ import particle.ParticleManager;
 public class MainUIController implements Initializable {
 
     // UI部品
-    @FXML
-    private AnchorPane displayPane;
+    @FXML private AnchorPane displayPane;
+    @FXML private Slider cameraX, cameraY, cameraZ, cameraRH, cameraRV;
 
     // 描画用
     private Timeline tl;
@@ -32,7 +34,11 @@ public class MainUIController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resource) {
         changeDBuilder(new Builder3D());
-        pmanager = new ParticleManager(0.1, 11, 11, 11);
+        pmanager = new ParticleManager(1.0, 11, 11, 11);
+        pmanager.setElectricField(-1.0E6, -1.0E6, -1.0E6);
+        pmanager.addElectron(0.01, 0.01, 0.01, 0.1, 0.1, 0.0);
+
+        setupUIComponets();
 
         // Timelineの初期化(0.5秒周期)
         tl = new Timeline(new KeyFrame(Duration.seconds(0.5), event -> {
@@ -40,6 +46,28 @@ public class MainUIController implements Initializable {
             dbuilder.update(pmanager.getParticles());
         }));
         tl.setCycleCount(Timeline.INDEFINITE);
+
+        tl.play();
+    }
+
+    /**
+     * UIのセットアップを行う
+     */
+    private void setupUIComponets() {
+        // 3Dカメラ
+        Runnable updateCamera3D = () -> {
+            double x = cameraX.getValue();
+            double y = cameraY.getValue();
+            double z = -cameraZ.getValue();
+            double rh = cameraRH.getValue();
+            double rv = cameraRV.getValue();
+            dbuilder.set3DCamera(x, y, z, rh, rv);
+        };
+        cameraX.valueProperty().addListener((__, oldV, newV) -> updateCamera3D.run());
+        cameraY.valueProperty().addListener((__, oldV, newV) -> updateCamera3D.run());
+        cameraZ.valueProperty().addListener((__, oldV, newV) -> updateCamera3D.run());
+        cameraRH.valueProperty().addListener((__, oldV, newV) -> updateCamera3D.run());
+        cameraRV.valueProperty().addListener((__, oldV, newV) -> updateCamera3D.run());
     }
 
     /**
