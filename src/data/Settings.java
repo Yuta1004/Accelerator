a2aae41f9d2d5d4ef1627f0e945d26dbc894a0af
NakagamiYuta <guguru0014@gmail.com>
2021-04-12 22:08:13 +0900

    [add] Settingsクラス(データクラス) 定義

---
+++ src/data/Settings.java
@@ -0,0 +1,18 @@
+package data;
+
+/**
+ * 設定値を扱うデータクラス
+ */
+public class Settings {
+
+    // 基本設定
+    public static double DT = 0.5;
+    public static double particleSize = 0.3;
+    public static double mouseSensitivity = 0.01;
+    public static double updateTickS = 600;
+
+    // 高度な設定
+    public static boolean normalizeAxis = true;
+    public static double meshN = 20;
+
+}
